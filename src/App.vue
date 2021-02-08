8d1cfdaab9d0e5540e12a21a95d8d46e07ec286c
DESKTOP-4EGO68O\Natawat <natawat14277@hotmail.com>
2021-02-09 06:52:45 +0700

    edit button

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,44 @@
   <v-app>
     <v-main>
       <router-view></router-view>
+      <a href="https://chat-api.one.th/go_api/api/v1/openScanQRcodeWithValue" target="_blank">
+      <font size=+4>Scan QR Code to Open Box</font>
+    </a>
+    <button>123456789</button>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+function oneChatCallBackQRScanner (ScanQRcodevalue) {
+  // alert(ScanQRcodevalue);
+  if (ScanQRcodevalue === '') {
+    alert('data empty ')
+  } else {
+    alert('Scan Success!')
+    alert(ScanQRcodevalue)
+  }
+}
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      qrtest: ''
+    }
+  },
+  methods: {
+    // oneChatCallBackQRScanner (ScanQRcodevalue) {
+    //   // alert(ScanQRcodevalue);
+    //   if (ScanQRcodevalue === '') {
+    //     alert('data empty ')
+    //   } else {
+    //     alert('Scan Success!')
+    //     alert(ScanQRcodevalue)
+    //   }
+    // }
+  },
+  updated () {
+    oneChatCallBackQRScanner()
+  }
 }
 </script>
