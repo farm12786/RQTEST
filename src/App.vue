891f7d9e73a20958b433d95efe4e65f70a4a8957
DESKTOP-4EGO68O\Natawat <natawat14277@hotmail.com>
2021-02-09 17:45:04 +0700

    dwf

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
   <v-app>
     <v-main>
       <router-view></router-view>
-      <div>REAL QR SCAN</div>
+      <a href="https://chat-api.one.th/go_api/api/v1/openScanQRcodeWithValue" target="_blank">
+      <font size=+4>Scan QR Code to Open Box</font>
+      </a>
+
+      <div>TESTTTTTTTTTTTT</div>
       <div id="qrcodecallback"></div>
       <div>this is result</div>
       <div>{{result}}</div>
